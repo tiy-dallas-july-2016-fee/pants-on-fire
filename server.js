147c1058ef6dd59175b1b44a61a3821b69f8059e
Eric Sowell <eric.sowell@gmail.com>
2016-09-27 16:08:03 -0500

    dynamic port

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+console.log('environment', process.env);
+console.log('my PWD', process.env.PWD);
+
 app.use(express.static('public'));
 
-app.listen(5500, function() {
-  console.log('listening on port 5500');
+var port = process.env.PORT || 5500;
+app.listen(port, function() {
+  console.log('listening on port', port);
 });
